c7e20e50af164cf5408a12dd59c99575c22c8007
andezitgq <sanerhp.mail@gmail.com>
2020-07-02 21:46:42 +0300

    Multiple Windows

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@ void print_middle(const char *msg);
 
 int main(int argc, char *argv[])
 {
+    WINDOW *list, *form;
+
     setlocale(LC_ALL, "");
 
     if (getuid()) {
@@ -23,22 +25,37 @@ int main(int argc, char *argv[])
     }
 
     initscr();
+
     int w, h;
     getmaxyx(stdscr, h, w);
 
-    if(w < 90 || h < 30) {
+    if(w < 70 || h < 20) {
         printf(FRED("Terminal windows is too small!\n"));
         endwin();
         return 0;
     }
+
+    list = newwin(h / 2 - 1, w - 2, 1, 1);
+    form = newwin(h / 2, w / 2, h / 2, w / 2);
+
     start_color();
     init_pair(1, COLOR_BLUE, COLOR_BLACK);
 
     box(stdscr, 0, 0);
+    box(list, 0, 0);
+    box(form, 0, 0);
     attron(COLOR_PAIR(1));
-    print_middle("netu");
+    print_middle("netu1");
     attroff(COLOR_PAIR(1));
-    refresh();
+    scrollok(list, TRUE);
+
+    for (int i = 0; i < 10; i++){
+        wprintw(list, "Govno");
+        wrefresh(list);
+    }
+    //refresh();
+    wrefresh(list);
+    wrefresh(form);
 
     pause();
     endwin();
